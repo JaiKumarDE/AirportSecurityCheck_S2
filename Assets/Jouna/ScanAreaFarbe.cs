aac34fd9f22240335a48420116fbaea8bc9bc26c
Jonah FUhrmeister <jonah.fuhrmeister08@htl-salzburg.ac.at>
2026-04-20 09:09:55 +0200

    Shaider test

---
+++ Assets/Jouna/ScanAreaFarbe.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScanAreaFarbe : MonoBehaviour
+{
+    public string scanAreaTag = "XRayArea";
+
+    private Renderer rend;
+    private MaterialPropertyBlock block;
+    private ScanColorManager manager;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        block = new MaterialPropertyBlock();
+
+        manager = FindObjectOfType<ScanColorManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(scanAreaTag)) return;
+
+        TagFarbe eintrag = manager.GetEintrag(gameObject.tag);
+        if (eintrag == null) return;
+
+        Color c = eintrag.farbe;
+        c.a = eintrag.alpha; // 👈 Alpha kommt jetzt vom Manager
+
+        block.SetColor("_Color", c);
+        rend.SetPropertyBlock(block);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag(scanAreaTag)) return;
+
+        block.Clear();
+        rend.SetPropertyBlock(block);
+    }
+}
---
+++ Assets/Jouna/ScanColorManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class TagFarbe
+{
+    public string tagName;
+    public Color farbe;
+    [Range(0f, 1f)] public float alpha = 0.5f; // 👈 NEU
+}
+
+public class ScanColorManager : MonoBehaviour
+{
+    public TagFarbe[] tagFarben;
+
+    public TagFarbe GetEintrag(string tag)
+    {
+        foreach (TagFarbe eintrag in tagFarben)
+        {
+            if (eintrag.tagName == tag)
+            {
+                return eintrag;
+            }
+        }
+
+        return null;
+    }
+}
